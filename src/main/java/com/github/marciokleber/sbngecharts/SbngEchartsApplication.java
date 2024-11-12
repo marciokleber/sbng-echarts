ffb5e69298b68b2a31c140582e5af7ffa68221dc
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-11-12 14:13:18 -0300

    :tada: First commit - criação de projeto spring-boot com angular usando a lib apache echarts

---
+++ src/main/java/com/github/marciokleber/sbngecharts/SbngEchartsApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.sbngecharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbngEchartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbngEchartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marciokleber/sbngecharts/SbngEchartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.sbngecharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbngEchartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
